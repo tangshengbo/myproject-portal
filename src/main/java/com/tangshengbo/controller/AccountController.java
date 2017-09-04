0a6c4fa87316eeacd0c832040bb79e1a0b1b4d38
tangshengbo <867850520tangbo@sian.com>
2017-09-04 23:12:44 +0800

    refactor spring mvc code

--- src/main/java/com/tangshengbo/controller/AccountController.java
+++ src/main/java/com/tangshengbo/controller/AccountController.java
@@ -1,18 +1,13 @@
 package com.tangshengbo.controller;
 
-import com.tangshengbo.model.Account;
-import com.tangshengbo.service.AccountService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 /**
  * Created by Administrator on 2016/11/16.
@@ -23,8 +18,7 @@ public class AccountController {
 
     private Log log = LogFactory.getLog(this.getClass());
 
-    @Autowired
-    private AccountService accountService;
+
 
     // 本方法将处理 /courses/view?courseId=123 形式的URL
     @RequestMapping(value = "/list", method = RequestMethod.POST)
@@ -34,9 +28,8 @@ public class AccountController {
         for (int i = 0; i < 10; i++) {
             log.info("getAccountAll start>>>>>>>>>>>>>>>>>>>>>>>>");
         }
-        List<Account> list = accountService.getAccountAll();
-        System.out.println(list.size());
-        model.addAttribute("accountList", list);
+
+
 
         log.info("getAccountAll end>>>>>>>>>>>>>>>>>>>>>>>>");
         return "account_list";
@@ -46,11 +39,6 @@ public class AccountController {
     @RequestMapping(value = "/staticpage.html", method = RequestMethod.POST)
     public ResponseEntity addAccount(Model model) {
         log.info("addAccount start>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        Account account = new Account();
-        account.setName("dubbo+spring");
-        account.setMoney(11.11);
-        accountService.addAccount(account);
 
         log.info("addAccount end>>>>>>>>>>>>>>>>>>>>>>>>");
         return new ResponseEntity("success", HttpStatus.OK);
