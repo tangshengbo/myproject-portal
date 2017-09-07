ebfd1cd09da5a70ea8f7699fde22df4197b39945
tangshengbo <867850520tangbo@sian.com>
2017-09-07 21:02:02 +0800

    add default implement

--- src/main/java/com/tangshengbo/controller/AccountController.java
+++ src/main/java/com/tangshengbo/controller/AccountController.java
@@ -1,14 +1,17 @@
 package com.tangshengbo.controller;
 
+import com.google.common.collect.Lists;
 import com.tangshengbo.model.Account;
 import com.tangshengbo.service.AccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,23 +28,25 @@ public class AccountController {
     private static Logger logger = LoggerFactory.getLogger(AccountController.class);
 
     @Autowired
+    @Qualifier("defaultAccountService")
     private AccountService accountService;
 
     // 本方法将处理 /courses/view?courseId=123 形式的URL
     @ResponseBody
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<Account> getAccountAll(Model model) {
-        logger.info("getAccountAll start>>>>>>>>>>>>>>>>>>>>>>>>");
-        logger.info("getAccountAll end>>>>>>>>>>>>>>>>>>>>>>>>");
         return accountService.findAll();
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/save/{count}", method = RequestMethod.GET)
+    public List<Account> saveAccount(@PathVariable("count") int count) {
+        accountService.saveBatchAccount(count);
+        return Lists.newArrayList();
+    }
     // 本方法将处理 /courses/view?courseId=123 形式的URL
     @RequestMapping(value = "/staticpage.html", method = RequestMethod.POST)
     public ResponseEntity addAccount(Model model) {
-        logger.info("addAccount start>>>>>>>>>>>>>>>>>>>>>>>>");
-
-        logger.info("addAccount end>>>>>>>>>>>>>>>>>>>>>>>>");
         return new ResponseEntity("success", HttpStatus.OK);
     }
 
---
+++ src/main/java/com/tangshengbo/service/impl/DefaultAccountServiceImpl.java
@@ -0,0 +1,40 @@
+package com.tangshengbo.service.impl;
+
+import com.tangshengbo.dao.AccountMapper;
+import com.tangshengbo.model.Account;
+import com.tangshengbo.service.AbstractService;
+import com.tangshengbo.service.AccountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by TangShengBo on 2017-09-07.
+ */
+@Service("defaultAccountService")
+public class DefaultAccountServiceImpl extends AbstractService<Account> implements AccountService {
+
+    private static Logger logger = LoggerFactory.getLogger(DefaultAccountServiceImpl.class);
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    @Override
+    public void saveBatchAccount(int batchCount) {
+        logger.info("DefaultImpl 批量插入开始.................");
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        logger.info("DefaultImpl 批量插入结束.................");
+    }
+
+    @Override
+    public void updateBatchAccount(int batchCount) {
+
+    }
+}
