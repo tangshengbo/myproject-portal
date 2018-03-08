096275e2cfc7a13b1baa45505e5cfa8d955d0034
tangshengbo <867850520tangbo@sian.com>
2018-03-08 21:24:14 +0800

    add image code

--- src/main/java/com/tangshengbo/controller/LoveImageController.java
+++ src/main/java/com/tangshengbo/controller/LoveImageController.java
@@ -1,6 +1,5 @@
 package com.tangshengbo.controller;
 
-import cn.hutool.core.io.FileTypeUtil;
 import com.alibaba.fastjson.JSON;
 import com.tangshengbo.core.BeanCopier;
 import com.tangshengbo.core.RandomUtils;
@@ -20,10 +19,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by Tangshengbo on 2018/3/6.
@@ -70,7 +72,7 @@ public class LoveImageController {
         logger.info("{}", url);
         String path = request.getServletContext().getRealPath("/");
         loveImageService.deleteImage(url);
-        return new File(path + url).delete()? "delete success": "delete fail";
+        return new File(path + url).delete() ? "delete success" : "delete fail";
     }
 
     @RequestMapping(value = "/load", method = RequestMethod.POST)
@@ -118,14 +120,14 @@ public class LoveImageController {
             String path = request.getServletContext().getRealPath(BASE_IMG_URL);
             //上传文件名
             String filename = file.getOriginalFilename();
-            String fileType = FileTypeUtil.getType(file.getInputStream());
-            if (!("jpg".equals(fileType) || "png".equals(fileType))) {
+            BufferedImage fileType = ImageIO.read(file.getInputStream());
+            if (Objects.isNull(fileType)) {
                 return "Please upload the picture!";
             }
-            File filepath = new File(path, filename);
+            File filePath = new File(path, filename);
             //判断路径是否存在，如果不存在就创建一个
-            if (!filepath.getParentFile().exists()) {
-                filepath.getParentFile().mkdirs();
+            if (!filePath.getParentFile().exists()) {
+                filePath.getParentFile().mkdirs();
             }
             //将上传文件保存到一个目标文件当中
             file.transferTo(new File(path + File.separator + filename));
