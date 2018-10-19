28fe87b4dc6e185ee2c215184a88a64ea52d8351
Tangshengbo <867850520tangbo@sina.com>
2018-10-19 18:40:30 +0800

    add listener code

--- src/main/java/com/tangshengbo/Application.java
+++ src/main/java/com/tangshengbo/Application.java
@@ -1,10 +1,15 @@
 package com.tangshengbo;
 
+import com.tangshengbo.model.HttpLog;
 import com.tangshengbo.model.MyClassPathXmlApplicationContext;
-import com.tangshengbo.model.PersonTest;
+import com.tangshengbo.service.LogService;
+import net.sf.cglib.beans.BeanMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.util.StopWatch;
+
+import java.util.List;
 
 /**
  * Created by TangShengBo on 2018/10/11
@@ -18,9 +23,22 @@ public class Application {
 //                new ClassPathXmlApplicationContext("");
 //                new XmlBeanFactory(new ClassPathResource("spring-context.xml"));
                 new MyClassPathXmlApplicationContext("spring-context.xml");
-//        LogService logService = (LogService) beanFactory.getBean("logService");
+        LogService logService = (LogService) beanFactory.getBean("logService");
 //        logger.info("{}", logService.listHttpLog());
-        PersonTest personTest = (PersonTest) beanFactory.getBean("personTest");
-        personTest.show();
+//        PersonTest personTest = (PersonTest) beanFactory.getBean("personTest");
+//        personTest.show();
+        List<HttpLog> httpLogList = logService.listHttpLog();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i = 0; i < 1000; i++) {
+            httpLogList.forEach(httpLog -> {
+                BeanMap beanMap =  BeanMap.create(httpLog);
+                logger.info("BeanMap:{}", beanMap);
+            });
+        }
+        stopWatch.stop();
+        logger.info("耗时：{}s", stopWatch.getTotalTimeSeconds());
+
+
     }
 }
---
+++ src/main/java/com/tangshengbo/model/MyNamespaceHandler.java
@@ -0,0 +1,14 @@
+package com.tangshengbo.model;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ * Created by Tangshengbo on 2018/10/19
+ */
+public class MyNamespaceHandler extends NamespaceHandlerSupport {
+
+    @Override
+    public void init() {
+
+    }
+}
