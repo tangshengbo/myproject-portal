33591cf1fea976382e7346bdc4cd269c2adae6d0
Tangshengbo <867850520tangbo@sina.com>
2019-01-09 20:46:21 +0800

    add response code

--- src/main/java/com/tangshengbo/controller/MyHandlerController.java
+++ src/main/java/com/tangshengbo/controller/MyHandlerController.java
@@ -1,7 +1,5 @@
 package com.tangshengbo.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.tangshengbo.core.ResponseGenerator;
 import com.tangshengbo.core.extension.MyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +8,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 
 /**
  * Created by TangShengBo on 2019/1/6
@@ -23,11 +20,16 @@ public class MyHandlerController implements MyHandler {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         logger.info("正在处理...................{}", request.getRequestURI());
-        response.setContentType("application/json;charset=UTF-8");
-        try (PrintWriter pw =  response.getWriter()) {
-            pw.print(JSON.toJSONString(ResponseGenerator.genSuccessResult("处理完成saas")));
-            pw.flush();
-        }
+//        response.setContentType("application/json;charset=UTF-8");
+//        try (PrintWriter pw =  response.getWriter()) {
+//            pw.print(JSON.toJSONString(ResponseGenerator.genSuccessResult("处理完成saas")));
+//            pw.flush();
+//        }
+//        RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
+//        rd.forward(request, response);
+        response.setStatus(302);
+        response.setHeader("Location", request.getContextPath() +"/index.jsp");
+//        response.sendRedirect(request.getContextPath() +"/index.jsp");
         return null;
     }
 }
