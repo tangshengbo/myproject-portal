a877b8642fd98e4571c1ab835ef9be2e4170ea3c
Tangshengbo <867850520tangbo@sina.com>
2019-01-25 17:35:49 +0800

    add rest controllerAdvice code

--- src/main/java/com/tangshengbo/service/GlobalExceptionHandler.java
+++ src/main/java/com/tangshengbo/service/GlobalExceptionHandler.java
@@ -12,25 +12,22 @@ import com.tangshengbo.exception.ServiceException;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     //处理自定义的异常
     @ExceptionHandler(ServiceException.class)
-    @ResponseBody
     public ResponseMessage customHandler(ServiceException e) {
         return ResponseGenerator.genFailResult(e.getMessage());
     }
 
     //其他未处理的异常
     @ExceptionHandler(Exception.class)
-    @ResponseBody
     public ResponseMessage exceptionHandler(Exception e) {
         logger.error("{}", ExceptionUtils.getStackTrace(e));
         return ResponseGenerator.genServerErrorResult(ExceptionUtils.getMessage(e));
